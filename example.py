ac5d13fa3ae437f5101d586b1b2c5983a3975a95
Emma Ai <emma.ai@ga.gov.au>
2023-10-24 09:53:10 +0000

    register models

--- example.py
+++ example.py
@@ -4,6 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
 
+import mlflow.sklearn
+from mlflow.models import infer_signature
+
 mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment(experiment_id="2")
 mlflow.autolog()
@@ -17,3 +20,13 @@ rf.fit(X_train, y_train)
 
 # Use the model to make predictions on the test dataset.
 predictions = rf.predict(X_test)
+signature = infer_signature(X_test, predictions)
+
+mlflow.sklearn.log_model(
+    sk_model=rf,
+    artifact_path="sklearn-model",
+    signature=signature,
+    registered_model_name="sk-learn-random-forest-reg-model",
+)
+
+print(f"default artifacts URI: '{mlflow.get_artifact_uri()}'")
